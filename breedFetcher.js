7eeaebd4efd98a80c0089165e9de8e9aee26a410
Netsanet <netsizebene@gmail.com>
2022-08-04 23:48:06 +0000

    adding test code

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,21 @@
 const request = require('request')
-const breedName = process.argv[2];
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
+const fetchBreedDescription= function (breedName, callback){
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 request (url, (err, response, body) => {
  
   if (err) {
-    console.log(`The request has failed. ${err}`)
+    return callback(err, null)
   }
   const data = JSON.parse(body);
   
   const breedDetails = data[0];
   if (breedDetails) {
-    console.log(breedDetails.description)
+    return callback(null,breedDetails.description)
   } else {
-    console.log("breed not found.");
+    callback("breed not found.");
   }
 })
 
+};
+module.exports={fetchBreedDescription};
